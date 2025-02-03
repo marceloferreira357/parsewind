8190f53a718392d67e60102ee9f5c7b69c49c915
Marcelo Ferreira <104363673+marceloferreira357@users.noreply.github.com>
2025-02-03 15:39:14 -0300

    feat: udpated the tests to include 3 classes

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -4,10 +4,10 @@ import { pw } from "./index";
 describe("pw function", () => {
   it("should return the joined class names", () => {
     const spy = jest.spyOn(classes, "get");
-    const result = pw("container aspect-square");
+    const result = pw("container aspect-square bottom-auto");
 
-    expect(result).toBe("width: 100%; aspect-ratio: 1 / 1;");
-    expect(spy).toHaveBeenCalledTimes(2);
+    expect(result).toBe("width: 100%; aspect-ratio: 1 / 1; bottom: auto;");
+    expect(spy).toHaveBeenCalledTimes(3);
 
     spy.mockRestore();
   });
